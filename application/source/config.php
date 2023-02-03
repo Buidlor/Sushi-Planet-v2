bf6f0eaafec1fb63c7d26a3a58ef4253787746b0
Buidlor <jefboone@gmail.com>
2023-02-03 09:07:00 +0100

    from laptop

--- application/source/config.php
+++ application/source/config.php
@@ -9,22 +9,4 @@
         echo $e->getMessage();
     }
 
-    // Read Json file
-    $json = file_get_contents('/data/contactform.json');
-    $json_data = json_decode($json, true);
-
-    // Prepare SQL statement
-    $sql = "INSERT INTO contactform (FirstName, SurName, Email, Subject, Message) VALUES (:FirstName, :SurName, :Email, :Subject, :Message)";
-    $stmt = $connection->prepare($sql);
-
-    // Loop through each data item and insert into database
-    foreach ($json_data as $item) {
-        $stmt->execute([
-            'FirstName' => $item['FirstName'],
-            'SurName' => $item['SurName'],
-            'Email' => $item['Email'],
-            'Subject' => $item['Subject'],
-            'Message' => $item['Message']
-        ]);
-    }
 ?>
